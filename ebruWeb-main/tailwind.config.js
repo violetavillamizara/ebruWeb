491c5f60829c51a9f8af8f6deec601e721b25b3a
O2F-IT\violeta.villamizarac <violeta.villamizaracuna@amaris.com>
2025-10-16 16:30:46 -0500

    Primer commit con estructura inicial

---
+++ ebruWeb-main/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./ebruWeb-main/**/*.html",
+    "./ebruWeb-main/scripts/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
